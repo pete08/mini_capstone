3f86be8bd513bc57934c1821b897e9ec6aa9b4e3
PeterH <pete08@gmail.com>
2020-07-05 10:21:47 -0500

    Add Update restful api route

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -6,12 +6,23 @@ class Api::ProductsController < ApplicationController
   end
 
   def create
-    @product = Prdocut.new( 
+    @product = Product.new( 
       name: params[:name], 
       description: params[:description], 
       price: params[:price], 
       img_url: params[:img_url]
   )
+    @product.save
     render "create.json.jb"
+  end
+
+  def update
+    @product = Product.find_by(id: params[:id])
+    @product.name = "newgame"
+    @product.price = 16
+    @product.image_url = "catanboardgame.com"
+    @product.description = "A game of land and resource management"
+    @product.save
+    render "update.json.jb"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   namespace :api do
     get "/products" => "products#index"
     post "/products" => "products#create"
+    patch "/products/:id" => "products#update"
   end
 
 end
