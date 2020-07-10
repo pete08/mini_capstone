1a2bd66764097e7523ba18a8db1b494c891a6318
PeterH <pete08@gmail.com>
2020-07-09 22:01:58 -0500

    Add Active Record using query url paramters to Index controller api request

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,7 +1,18 @@
 class Api::ProductsController < ApplicationController
 
   def index
-    @products = Product.all
+    #1. Change the index action to allow for searching by name (using params “search”).
+    # @products = Product.where("name LIKE ?", "%#{params[:search]}%")
+    #2. - Change the index action to allow the user to display all products under $2.00 or some other price of your choosing (using params “discount” equal to “true”).
+    # discount = 20
+    # if params[:discount]
+    #   @products = Product.where("price < #{discount}")
+    # else
+    #   @products = Product.all
+    # end
+
+    
+    # @products = Product.all
     render "index.json.jb"
   end
 
